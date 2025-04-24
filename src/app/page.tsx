c35a810f69771cbb51d2ff3f1da848aa384a3487
devnkz <vieiranyckolas1@gmail.com>
2025-04-24 08:43:18 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-zinc-600">
-      <h1 className="font-bold text-white text-center text-7xl pt-36">Bem vindo a lista de filmes recentes</h1>
+    <div className="h-screen w-full bg-zinc-200">
+      <header className="w-full p-4 flex justify-around items-center bg-black text-red-600 font-bold">
+          <h1 className="text-md">Lista Filmes</h1>
+          <h2 className="text-sm">Desenvolvido por nyckolas</h2>
+      </header>
     </div>
   );
 }
